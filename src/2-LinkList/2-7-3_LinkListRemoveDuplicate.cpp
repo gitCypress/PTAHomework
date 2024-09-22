570ae8b85284e3b2bcc025df7adec55b01a000d5
LapCypress <tencypress@qq.com>
2024-09-22 21:51:23 +0800

    自己实现的版本并且不用容器

--- src/2-LinkList/2-7-3_LinkListRemoveDuplicate.cpp
+++ src/2-LinkList/2-7-3_LinkListRemoveDuplicate.cpp
@@ -2,3 +2,148 @@
 // Created by DesCypress on 24-9-22.
 // 7-3 链表去重
 // 使用一个大数组的地址直接作为链表存储数据完成链接，有点像内存池这是可以说的吗
+// !!注意多个表的时候有没有用叉
+// !!一定要想清楚循环条件，这里就是表跑完了不匹配才加，而不是只要不匹配就加，是比较特殊的情况
+// !!下面这种实现和真正的链表有区别，映射不共享，数据也要记得搬过去
+// 这种方法创建的本身就是一个内存空间，意味着着不能随便去创建新空间，尤其是这里的 wasted
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+struct Node {
+    int data = 0;
+    int next = -1;
+};
+
+int head = 100000;  // 主链头节点
+int length;  // 主链输入长度
+
+int wastedHead = 100001;  // 丢弃链节点
+
+
+void input(Node* list);
+void print(Node* list, int HEAD);
+void printArr(Node* list, int* arr, int LENGTH);
+
+int main() {
+    // 自建内存
+    Node* list = new Node[100001];  // 主链
+
+    // 处理输入
+    input(list);
+
+    // 核心逻辑
+    int fp = head;  // 指向头节点的临时指针 former point
+    int cp = list[head].next;  // 等于头指针的临时指针 current point
+
+    int set[length];
+    int setLength = 0;  // 记录比较过了的值，并且保留它的长度
+
+    int wasted[length];
+    int wastedLength = 0;  // 不需要也不能直接new一个list，这样会导致映射关系不统一
+
+    while (cp != -1) {  // 遍历主链
+
+        int listData = list[cp].data;  // 取一个数
+
+        bool is_nobody = true;  // 是否跑完了整个记录值表都没跑出来
+
+        for (int i = 0; i < length; i++) {  // 遍历记录值，listData主链元素和记录值依次比较
+
+            int setData = set[i];  // 取数
+
+            int cpn = list[cp].next;// 备份所有 自身会被被修改 的临时指针的 next，后面确定不用了再删
+
+            if(abs(listData) == abs(setData)) {  // 如果相等，就需要将当前节点从主链删除，添加到到废弃链尾部
+
+
+                list[fp].next = cpn;
+                list[cp].next = -1;  // 标准删除操作
+
+                wasted[wastedLength] = cp;  // 特殊添加操作，需要复制数据
+                wastedLength++;
+
+                // 更新操作
+                fp = fp;  // 删除操作 fp不动
+                cp = cpn;  // cp 在原链后移
+
+                is_nobody = false;  // 有修改说明有一样的
+                break;  // 不用再看后面了
+            }
+        }
+
+        if (is_nobody) {  // 没有相等的值则不修改指针，只自更新主链，并把值添加到记录表
+            fp = list[fp].next;
+            cp = list[cp].next;
+
+            set[setLength] = listData;  // 长度即为添加到尾部的下标
+            setLength++;
+        }
+
+    };
+
+    print(list, head);
+    printArr(list, wasted, wastedLength);
+
+    delete list;
+}
+
+void input(Node* list) {
+
+    cin >> list[head].next >> length;
+
+    for (int i = 0; i < length; i++) {
+        int addr_, next_, data_;
+        cin >> addr_ >> data_ >> next_;
+        list[addr_].data = data_;
+        list[addr_].next = next_;
+    }
+}
+
+void print(Node* list, int HEAD) {
+
+    int current = list[HEAD].next;  // 临时指针
+    while(current != -1) {  // 遍历，所以读到尾部即可
+
+        // 不修改，只涉及读取
+        stringstream sAddr, sNext;
+        sAddr << setw(5) << setfill('0') << current;
+        sNext << setw(5) << setfill('0') << list[current].next;
+
+        cout << sAddr.str() << " "
+             << list[current].data << " ";
+
+        if (list[current].next == -1)
+            cout << "-1" << endl;
+        else
+            cout << sNext.str() << endl;
+
+        // 更新临时指针
+        current = list[current].next;
+    }
+
+}
+
+void printArr(Node* list, int* arr, int LENGTH) {
+
+    for (int i = 0; i < LENGTH ; i++) {  // 遍历，所以读到尾部即可
+
+        // 不修改，只涉及读取
+        stringstream sAddr;
+        sAddr << setw(5) << setfill('0') << arr[i];
+
+        cout << sAddr.str() << " "
+             << list[arr[i]].data << " ";
+
+        if (i == LENGTH - 1)
+            cout << "-1" << endl;
+        else {
+            stringstream sNext;
+            sNext << setw(5) << setfill('0') << arr[i+1];
+            cout << sNext.str() << endl;
+        }
+    }
+}
+
--- src/2-LinkList/2-7-4_LinkListReArrangement.cpp
+++ src/2-LinkList/2-7-4_LinkListReArrangement.cpp
@@ -1,5 +1,159 @@
 //
 // Created by DesCypress on 24-9-22.
 // 7-4 链表重排
+// tips:
+// 这里面 Ls是自己的地址，da是数据，Rs是 next
+
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int length = 0;  // 输入节点（有效）个数
+int startSerial;  // 起始地址
+int beforeHalf = 0;  // 要把前半和后半分开，因为是以 1作结尾
+
+struct Node {
+    int addr = -1;
+    int data = 0;
+    int next = -1;
+};
+
+Node* input();  // 将输入转换成内存池
+void print(Node* pool, int current = startSerial);  // 打印 “链表”, start 是头指针
+int halfserial(Node* pool);  // 获取中点位
+
+int main() {
+
+    // 处理输入
+    cin >> startSerial >> length;
+    Node* pool = input();
+
+    // N = 2 特例
+    if (pool[pool[startSerial].next].next == -1) {
+        int arr[2] = {pool[startSerial].addr, startSerial};
+
+            std::stringstream ssL, ssL2;
+            ssL << setw(5) << setfill('0') << pool[arr[0]].addr;
+            ssL2 << setw(5) << setfill('0') << pool[arr[1]].next;
+
+            cout << ssL2.str() << " "
+                 << pool[pool[startSerial].next].data << " "
+                 << ssL.str() << endl;
+
+            cout << ssL.str() << " "
+                 << pool[arr[0]].data << " "
+                 << "-1" << endl;
+
+        delete [] pool;
+        return 0;
+    };
+
+    int half = halfserial(pool);
+    pool[beforeHalf].next = -1; // 用 -1 标志分割前半和后半
 
 
+    // 倒转后半部分，以 456为例
+    int former = half;  // 4
+    int current = pool[half].next;  // 5
+    pool[former].next = -1;  // 实际末尾的 4->5 变为 4->null
+    int label = 0;  // 倒二标记
+    // cout << "former: " << pool[former].data << " current:" << pool[current].data << endl;
+
+    while (true) {
+        int next = pool[current].next;  // 存储 6 地址
+        pool[current].next = former;  // 使得 5-> 6 变成 5-> 4
+        former = current;  // 更新现指针为 6
+        current = next;  // 更新前指针为 5
+        // cout << "--former: " << pool[former].data << " current:" << pool[current].data << endl;
+        if (label == 1) break;  // 判断是否为倒数第二次
+        if (pool[current].next == -1) label = 1;  //abc, ba c, cba，可以看到，停止的条件必须在倒数第二次时知道
+
+    }
+
+
+    int lastSerial = former;  // 这时的 former是数组末尾数字；
+    pool[beforeHalf].next = -1;
+
+    // 交叉合并 543,(0)12 为 (0)51423  // 核心思路是 找停止条件-定位-修改-更新-修改-更新-。。。-收尾
+    // 偶数：zap，azp=null
+    // 奇数
+
+    // 创建头节点
+    pool[100000].next = startSerial;
+    startSerial = 100000;
+
+    int ap = startSerial, zp = lastSerial;  // 2 4 // 654 (0)123  (0)615243
+
+    while(true) {
+        int apn = pool[ap].next; // 3
+        int zpn = pool[zp].next; // null
+
+        if (apn == -1) {
+            pool[ap].next = zp;
+            break;
+        }
+
+        pool[zp].next = apn;
+        pool[ap].next = zp;
+        ap = apn;
+        zp = zpn;
+
+        if (zpn == -1) break;
+    }
+
+    print(pool, pool[startSerial].next);
+
+    delete [] pool;
+}
+
+Node* input() {
+
+    Node* output = new Node[100001];  // 左地址做下标
+
+    for (int i = 0; i < length; i++) {
+        int L, R; int d = 0;
+        cin >> L >> d >> R;
+        output[L].addr = L;
+        output[L].data = d;
+        output[L].next = R;
+    }
+
+    return output;
+}
+
+void print(Node* pool, int current) {
+
+    while (current != -1) {
+
+        // stringstream 格式化
+        std::stringstream ssL, ssR;
+        ssL << setw(5) << setfill('0') << pool[current].addr;
+        ssR << setw(5) << setfill('0') << pool[current].next;
+
+        cout << ssL.str() << " "
+             << pool[current].data << " ";
+
+        if (pool[current].next == -1) {
+            cout << -1 << endl;
+        }
+        else cout << ssR.str() << endl;
+
+        current = pool[current].next;
+    }
+}
+
+int halfserial(Node* pool) {
+
+    int slow = startSerial, fast = startSerial;
+
+    while (fast != -1 && pool[fast].next != -1) {  // 快指针的判断有两个，因为有可能快指针下次直接跳进未定义区域或着快指针已经到最后一位了
+        beforeHalf = slow;
+        slow = pool[slow].next;
+        fast = pool[pool[fast].next].next;
+        // cout << "slow: " << pool[slow].data << ", fast: " << pool[fast].data << endl;
+    }
+
+    return slow;
+};
