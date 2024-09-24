cbe622d5311da8e2a6aef868f62b5a3d60e39a86
LapCypress <tencypress@qq.com>
2024-09-24 21:08:40 +0800

    题目说明更新

--- src/2-LinkList/2-7-3_LinkListRemoveDuplicate.cpp
+++ src/2-LinkList/2-7-3_LinkListRemoveDuplicate.cpp
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
 struct Node {
@@ -20,12 +22,13 @@ struct Node {
 int head = 100000;  // 主链头节点
 int length;  // 主链输入长度
 
-int wastedHead = 100001;  // 丢弃链节点
-
+unordered_set<int> mySet;
+vector<int> mainVec;
+vector<int> wastedVec;
 
 void input(Node* list);
 void print(Node* list, int HEAD);
-void printArr(Node* list, int* arr, int LENGTH);
+void printArr(Node* list, vector<int> arr);
 
 int main() {
     // 自建内存
@@ -34,58 +37,7 @@ int main() {
     // 处理输入
     input(list);
 
-    // 核心逻辑
-    int fp = head;  // 指向头节点的临时指针 former point
-    int cp = list[head].next;  // 等于头指针的临时指针 current point
-
-    int set[length];
-    int setLength = 0;  // 记录比较过了的值，并且保留它的长度
-
-    int wasted[length];
-    int wastedLength = 0;  // 不需要也不能直接new一个list，这样会导致映射关系不统一
-
-    while (cp != -1) {  // 遍历主链
-
-        int listData = list[cp].data;  // 取一个数
-
-        bool is_nobody = true;  // 是否跑完了整个记录值表都没跑出来
-
-        for (int i = 0; i < length; i++) {  // 遍历记录值，listData主链元素和记录值依次比较
-
-            int setData = set[i];  // 取数
-
-            int cpn = list[cp].next;// 备份所有 自身会被被修改 的临时指针的 next，后面确定不用了再删
-
-            if(abs(listData) == abs(setData)) {  // 如果相等，就需要将当前节点从主链删除，添加到到废弃链尾部
-
-
-                list[fp].next = cpn;
-                list[cp].next = -1;  // 标准删除操作
-
-                wasted[wastedLength] = cp;  // 特殊添加操作，需要复制数据
-                wastedLength++;
-
-                // 更新操作
-                fp = fp;  // 删除操作 fp不动
-                cp = cpn;  // cp 在原链后移
-
-                is_nobody = false;  // 有修改说明有一样的
-                break;  // 不用再看后面了
-            }
-        }
-
-        if (is_nobody) {  // 没有相等的值则不修改指针，只自更新主链，并把值添加到记录表
-            fp = list[fp].next;
-            cp = list[cp].next;
-
-            set[setLength] = listData;  // 长度即为添加到尾部的下标
-            setLength++;
-        }
-
-    };
-
-    print(list, head);
-    printArr(list, wasted, wastedLength);
+    printArr(list, mainVec);
 
     delete list;
 }
@@ -99,6 +51,16 @@ void input(Node* list) {
         cin >> addr_ >> data_ >> next_;
         list[addr_].data = data_;
         list[addr_].next = next_;
+
+        if (mySet.find(abs(data_)) != mySet.end()) {
+            wastedVec.push_back(addr_);
+        }
+        else {
+            mainVec.push_back(addr_);
+        }
+
+        mySet.insert(abs(data_));
+
     }
 }
 
@@ -126,9 +88,9 @@ void print(Node* list, int HEAD) {
 
 }
 
-void printArr(Node* list, int* arr, int LENGTH) {
+void printArr(Node* list, vector<int> arr) {
 
-    for (int i = 0; i < LENGTH ; i++) {  // 遍历，所以读到尾部即可
+    for (size_t i = 0; i < arr.size() - 1; ++i) {  // 遍历，所以读到尾部即可
 
         // 不修改，只涉及读取
         stringstream sAddr;
@@ -137,7 +99,7 @@ void printArr(Node* list, int* arr, int LENGTH) {
         cout << sAddr.str() << " "
              << list[arr[i]].data << " ";
 
-        if (i == LENGTH - 1)
+        if (i == arr.size() - 2)
             cout << "-1" << endl;
         else {
             stringstream sNext;
