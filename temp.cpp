0120a0d8b32f9db092faaf0117e20d945fa4252a
SerCypress <tencypress@qq.com>
2024-10-06 05:39:03 +0000

    换用cmake，更新服务端配置文件

--- temp.cpp
+++ temp.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
 using namespace std;
 
-const int MAXSIZE = 100;
-enum {SUCCESS, FAIL};
-
-struct Stack{
-    int data[MAXSIZE];
-    int top = -1;
-};
-
-int push(Stack& stack, int data){
-    if (MAXSIZE == stack.top + 1) return FAIL;
-    stack.top++;
-}
-
 int main(){
-    Stack test;
-
+    cout << "here?" << endl;
 }
